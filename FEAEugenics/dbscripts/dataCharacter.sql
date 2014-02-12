eed5c302bfc0817347f70ba16b9f3060f4b75cd7
Joe Kearney <jkearney486@gmail.com>
2014-02-12 07:55:56 -0500

    add in the sql scripts for building the db
    mostly just the data, although I should get the sql for making the FKs
    huh

---
+++ FEAEugenics/dbscripts/dataCharacter.sql
@@ -0,0 +1,84 @@
+--//													  Str,Mag,Skl,Spd,Lck,Def,Res
+insert into [Character] values ('Avatar (M)', 'Male',		0,	0,	0,	0,	0,	0,	0)
+insert into [Character] values ('Avatar (F)', 'Female',		0,	0,	0,	0,	0,	0,	0)
+insert into [Character] values ('Chrom', 'Male',			1,	0,	1,	1,	1, -1, -1)
+insert into [Character] values ('Lissa', 'Female',			-2,	2, -1,	1,	1, -1, -1)
+insert into [Character] values ('Frederick', 'Male',		2, -2,	2, -2,	0,	2,	0)
+insert into [Character] values ('Sully', 'Female',			-1,	-1,	2,	2,	0,	-1,	0)
+insert into [Character] values ('Virion', 'Male',			0,	0,	2,	2,	-1,	-2,	0)
+insert into [Character] values ('Stahl', 'Male',			2,	-1,	1,	0,	-2,	2,	-1)
+insert into [Character] values ('Vaike', 'Male',			3,	-2,	1,	1,	-1,	0,	-2)
+insert into [Character] values ('Miriel', 'Female',			-2,	3,	1,	1,	0,	-2,	0)
+insert into [Character] values ('Sumia', 'Female',			-2,	0,	2,	3,	0,	-2,	1)
+insert into [Character] values ('Kellam', 'Male',			1,	0,	1,	-2,	-2,	3,	0)
+insert into [Character] values ('Donnel', 'Male',			1,	-1,	-1,	-1,	3,	1,	-1)
+insert into [Character] values ('Lon''qu', 'Male',			0,	0,	3,	3,	0,	-2,	-2)
+insert into [Character] values ('Ricken', 'Male',			-1,	2,	0,	0,	1,	-1,	0)
+insert into [Character] values ('Maribelle', 'Female',		-3,	2,	1,	0,	3,	-3,	2)
+insert into [Character] values ('Panne', 'Female',			2,	-1,	2,	3,	-1,	1,	-1)
+insert into [Character] values ('Gaius', 'Male',			1,	-1,	2,	2,	-2,	-1,	0)
+insert into [Character] values ('Cordelia', 'Female',		1,	-1,	2,	2,	-1,	0,	-1)
+insert into [Character] values ('Gregor', 'Male',			2,	-1,	2,	0,	-1,	1,	-2)
+insert into [Character] values ('Nowi', 'Female',			1,	1,	-1,	-2,	1,	3,	2)
+insert into [Character] values ('Libra', 'Male',			0,	1,	1,	0,	-1,	0,	1)
+insert into [Character] values ('Tharja', 'Female',			0,	3,	-1,	1,	-3,	1,	0)
+insert into [Character] values ('Anna', 'Female',			-1,	0,	1,	0,	3,	-1,	0)
+insert into [Character] values ('Olivia', 'Female',			0,	0,	1,	1,	0,	-1,	-1)
+insert into [Character] values ('Cherche', 'Female',		3,	0,	-1,	-1,	0,	0,	0)
+insert into [Character] values ('Henry', 'Male',			1,	1,	2,	0,	-2,	1,	-1)
+insert into [Character] values ('Say''ri', 'Female',		1,	-1,	1,	1,	-1,	0,	1)
+insert into [Character] values ('Tiki', 'Female',			0,	-1,	0,	1,	2,	1,	2)
+insert into [Character] values ('Basilio', 'Male',			3,	-2,	1,	1,	-1,	1,	-1)
+insert into [Character] values ('Flavia', 'Female',			1,	-1,	1,	1,	0,	-1,	0)
+insert into [Character] values ('Gangrel', 'Male',			-2,	0,	3,	3,	-1,	-1,	0)
+insert into [Character] values ('Walhart', 'Male',			4,	-2,	0,	-1,	-1,	4,	-2)
+insert into [Character] values ('Emmeryn', 'Female',		-2,	4,	0,	1,	0,	-2,	-1)
+insert into [Character] values ('Yen''fay', 'Male',			1,	-2,	2,	4,	0,	-1,	-2)
+insert into [Character] values ('Aversa', 'Female',			-1,	3,	1,	1,	-2,	0,	0)
+insert into [Character] values ('Priam', 'Male',			3,	-2,	1,	0,	0,	2,	-2)
+insert into [Character] values ('Morgan (M)', 'Male',		1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Morgan (F)', 'Female',		1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Lucina', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Owain', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Inigo', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Brady', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Kjelle', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Cynthia', 'Female',		1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Severa', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Gerome', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Yarne', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Laurent', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Noire', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] values ('Nah', 'Female',			1,	1,	1,	1,	1,	1,	1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
