b8c714c8e90a7f1698fae903c8477151568717bd
Joe Kearney <jkearney486@gmail.com>
2014-05-06 11:05:07 -0400

    update the sql script
    Added identity insert for script that populates the Character table, so
    that those IDs can be relied on for the later scripts like ClassSet and
    Support

--- FEAEugenics/dbscripts/dataCharacter.sql
+++ FEAEugenics/dbscripts/dataCharacter.sql
@@ -1,84 +1,57 @@
---//													  Str,Mag,Skl,Spd,Lck,Def,Res
-insert into [Character] values ('Avatar (M)', 'Male',		0,	0,	0,	0,	0,	0,	0)
-insert into [Character] values ('Avatar (F)', 'Female',		0,	0,	0,	0,	0,	0,	0)
-insert into [Character] values ('Chrom', 'Male',			1,	0,	1,	1,	1, -1, -1)
-insert into [Character] values ('Lissa', 'Female',			-2,	2, -1,	1,	1, -1, -1)
-insert into [Character] values ('Frederick', 'Male',		2, -2,	2, -2,	0,	2,	0)
-insert into [Character] values ('Sully', 'Female',			-1,	-1,	2,	2,	0,	-1,	0)
-insert into [Character] values ('Virion', 'Male',			0,	0,	2,	2,	-1,	-2,	0)
-insert into [Character] values ('Stahl', 'Male',			2,	-1,	1,	0,	-2,	2,	-1)
-insert into [Character] values ('Vaike', 'Male',			3,	-2,	1,	1,	-1,	0,	-2)
-insert into [Character] values ('Miriel', 'Female',			-2,	3,	1,	1,	0,	-2,	0)
-insert into [Character] values ('Sumia', 'Female',			-2,	0,	2,	3,	0,	-2,	1)
-insert into [Character] values ('Kellam', 'Male',			1,	0,	1,	-2,	-2,	3,	0)
-insert into [Character] values ('Donnel', 'Male',			1,	-1,	-1,	-1,	3,	1,	-1)
-insert into [Character] values ('Lon''qu', 'Male',			0,	0,	3,	3,	0,	-2,	-2)
-insert into [Character] values ('Ricken', 'Male',			-1,	2,	0,	0,	1,	-1,	0)
-insert into [Character] values ('Maribelle', 'Female',		-3,	2,	1,	0,	3,	-3,	2)
-insert into [Character] values ('Panne', 'Female',			2,	-1,	2,	3,	-1,	1,	-1)
-insert into [Character] values ('Gaius', 'Male',			1,	-1,	2,	2,	-2,	-1,	0)
-insert into [Character] values ('Cordelia', 'Female',		1,	-1,	2,	2,	-1,	0,	-1)
-insert into [Character] values ('Gregor', 'Male',			2,	-1,	2,	0,	-1,	1,	-2)
-insert into [Character] values ('Nowi', 'Female',			1,	1,	-1,	-2,	1,	3,	2)
-insert into [Character] values ('Libra', 'Male',			0,	1,	1,	0,	-1,	0,	1)
-insert into [Character] values ('Tharja', 'Female',			0,	3,	-1,	1,	-3,	1,	0)
-insert into [Character] values ('Anna', 'Female',			-1,	0,	1,	0,	3,	-1,	0)
-insert into [Character] values ('Olivia', 'Female',			0,	0,	1,	1,	0,	-1,	-1)
-insert into [Character] values ('Cherche', 'Female',		3,	0,	-1,	-1,	0,	0,	0)
-insert into [Character] values ('Henry', 'Male',			1,	1,	2,	0,	-2,	1,	-1)
-insert into [Character] values ('Say''ri', 'Female',		1,	-1,	1,	1,	-1,	0,	1)
-insert into [Character] values ('Tiki', 'Female',			0,	-1,	0,	1,	2,	1,	2)
-insert into [Character] values ('Basilio', 'Male',			3,	-2,	1,	1,	-1,	1,	-1)
-insert into [Character] values ('Flavia', 'Female',			1,	-1,	1,	1,	0,	-1,	0)
-insert into [Character] values ('Gangrel', 'Male',			-2,	0,	3,	3,	-1,	-1,	0)
-insert into [Character] values ('Walhart', 'Male',			4,	-2,	0,	-1,	-1,	4,	-2)
-insert into [Character] values ('Emmeryn', 'Female',		-2,	4,	0,	1,	0,	-2,	-1)
-insert into [Character] values ('Yen''fay', 'Male',			1,	-2,	2,	4,	0,	-1,	-2)
-insert into [Character] values ('Aversa', 'Female',			-1,	3,	1,	1,	-2,	0,	0)
-insert into [Character] values ('Priam', 'Male',			3,	-2,	1,	0,	0,	2,	-2)
-insert into [Character] values ('Morgan (M)', 'Male',		1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Morgan (F)', 'Female',		1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Lucina', 'Female',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Owain', 'Male',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Inigo', 'Male',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Brady', 'Male',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Kjelle', 'Female',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Cynthia', 'Female',		1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Severa', 'Female',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Gerome', 'Male',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Yarne', 'Male',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Laurent', 'Male',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Noire', 'Female',			1,	1,	1,	1,	1,	1,	1)
-insert into [Character] values ('Nah', 'Female',			1,	1,	1,	1,	1,	1,	1)
+set identity_insert [Character] on
 
+--//																																								  Str,Mag,Skl,Spd,Lck,Def,Res
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (1, 'Avatar (M)', 'Male',		0,	0,	0,	0,	0,	0,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (2, 'Avatar (F)', 'Female',		0,	0,	0,	0,	0,	0,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (3, 'Chrom', 'Male',				1,	0,	1,	1,	1, -1, -1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (4, 'Lissa', 'Female',			-2,	2, -1,	1,	1, -1, -1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (5, 'Frederick', 'Male',			2, -2,	2, -2,	0,	2,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (6, 'Sully', 'Female',			-1,	-1,	2,	2,	0,	-1,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (7, 'Virion', 'Male',			0,	0,	2,	2,	-1,	-2,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (8, 'Stahl', 'Male',				2,	-1,	1,	0,	-2,	2,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (9, 'Vaike', 'Male',				3,	-2,	1,	1,	-1,	0,	-2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (10, 'Miriel', 'Female',			-2,	3,	1,	1,	0,	-2,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (11, 'Sumia', 'Female',			-2,	0,	2,	3,	0,	-2,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (12, 'Kellam', 'Male',			1,	0,	1,	-2,	-2,	3,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (13, 'Donnel', 'Male',			1,	-1,	-1,	-1,	3,	1,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (14, 'Lon''qu', 'Male',			0,	0,	3,	3,	0,	-2,	-2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (15, 'Ricken', 'Male',			-1,	2,	0,	0,	1,	-1,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (16, 'Maribelle', 'Female',		-3,	2,	1,	0,	3,	-3,	2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (17, 'Panne', 'Female',			2,	-1,	2,	3,	-1,	1,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (18, 'Gaius', 'Male',			1,	-1,	2,	2,	-2,	-1,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (19, 'Cordelia', 'Female',		1,	-1,	2,	2,	-1,	0,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (20, 'Gregor', 'Male',			2,	-1,	2,	0,	-1,	1,	-2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (21, 'Nowi', 'Female',			1,	1,	-1,	-2,	1,	3,	2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (22, 'Libra', 'Male',			0,	1,	1,	0,	-1,	0,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (23, 'Tharja', 'Female',			0,	3,	-1,	1,	-3,	1,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (24, 'Anna', 'Female',			-1,	0,	1,	0,	3,	-1,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (25, 'Olivia', 'Female',			0,	0,	1,	1,	0,	-1,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (26, 'Cherche', 'Female',		3,	0,	-1,	-1,	0,	0,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (27, 'Henry', 'Male',			1,	1,	2,	0,	-2,	1,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (28, 'Say''ri', 'Female',		1,	-1,	1,	1,	-1,	0,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (29, 'Tiki', 'Female',			0,	-1,	0,	1,	2,	1,	2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (30, 'Basilio', 'Male',			3,	-2,	1,	1,	-1,	1,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (31, 'Flavia', 'Female',			1,	-1,	1,	1,	0,	-1,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (32, 'Gangrel', 'Male',			-2,	0,	3,	3,	-1,	-1,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (33, 'Walhart', 'Male',			4,	-2,	0,	-1,	-1,	4,	-2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (34, 'Emmeryn', 'Female',		-2,	4,	0,	1,	0,	-2,	-1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (35, 'Yen''fay', 'Male',			1,	-2,	2,	4,	0,	-1,	-2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (36, 'Aversa', 'Female',			-1,	3,	1,	1,	-2,	0,	0)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (37, 'Priam', 'Male',			3,	-2,	1,	0,	0,	2,	-2)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (38, 'Morgan (M)', 'Male',		1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (39, 'Morgan (F)', 'Female',		1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (40, 'Lucina', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (41, 'Owain', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (42, 'Inigo', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (43, 'Brady', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (44, 'Kjelle', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (45, 'Cynthia', 'Female',		1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (46, 'Severa', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (47, 'Gerome', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (48, 'Yarne', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (49, 'Laurent', 'Male',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (50, 'Noire', 'Female',			1,	1,	1,	1,	1,	1,	1)
+insert into [Character] ([CharacterID], [Name], [Gender], [ModStr], [ModMag], [ModSkl], [ModSpd], [ModLck], [ModDef], [ModRes]) values (51, 'Nah', 'Female',			1,	1,	1,	1,	1,	1,	1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+set identity_insert [Character] off
 
